37d3c71e76d6726de121cee24dc8494f548b4b65
Łukasz Mazurkiewicz <mazurkiewicz_l@outlook.com>
2020-09-23 21:31:10 +0200

    MOVE state from square to board component

--- src/index.js
+++ src/index.js
@@ -15,16 +15,34 @@ class Square extends React.Component {
     return (
       <button 
         className="square" 
-        onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
+        onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'x';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square 
+        value={this.state.squares[i]} 
+        onClick={() => this.handleClick(i)}
+      />
+      );
   }
 
   render() {
