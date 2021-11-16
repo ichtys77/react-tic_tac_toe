6824f490680f69fdeb8710d9109e637909efe9be
Łukasz Mazurkiewicz <mazurkiewicz_l@outlook.com>
2021-11-16 19:13:49 +0100

    Refresh packages

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'x';
+    squares[i] = 'o';
     this.setState({squares: squares});
   }
 
